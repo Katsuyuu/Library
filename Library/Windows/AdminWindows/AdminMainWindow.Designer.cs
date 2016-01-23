ee12a240ba8e359a31ef16584db947474d671489
Adrian <goonygogo@gmail.com>
2016-01-23 18:54:44 +0100

    Reszta okienek
    Jednak kłamałem, tutaj reszta okienek :)

--- Library/Windows/AdminWindows/AdminMainWindow.Designer.cs
+++ Library/Windows/AdminWindows/AdminMainWindow.Designer.cs
@@ -37,6 +37,7 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -44,11 +45,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 39);
-            this.label1.Name = "LabelPanelZarzadzania";
-            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.Location = new System.Drawing.Point(29, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Panel zarządzania";
+            this.label1.Text = "Dodaj:";
             // 
             // groupBox1
             // 
@@ -56,7 +57,7 @@
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(32, 55);
+            this.groupBox1.Location = new System.Drawing.Point(32, 39);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(142, 204);
             this.groupBox1.TabIndex = 1;
@@ -65,44 +66,44 @@
             // button4
             // 
             this.button4.Location = new System.Drawing.Point(15, 144);
-            this.button4.Name = "ButtonUzytkownicy";
+            this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(112, 35);
             this.button4.TabIndex = 3;
-            this.button4.Text = "Użytkownicy";
+            this.button4.Text = "Użytkownika";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(15, 103);
-            this.button3.Name = "ButtonWydawnictwa";
+            this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(112, 35);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Wydawnictwa";
+            this.button3.Text = "Wydawnictwo";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(15, 62);
-            this.button2.Name = "ButtonAutorzy";
+            this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Autorzy";
+            this.button2.Text = "Autora";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(15, 21);
-            this.button1.Name = "ButtonKsiazki";
+            this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Książki";
+            this.button1.Text = "Książkę";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(47, 265);
-            this.button5.Name = "ButtonKontrolaWypozyczen";
-            this.button5.Size = new System.Drawing.Size(221, 48);
+            this.button5.Location = new System.Drawing.Point(32, 262);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 48);
             this.button5.TabIndex = 2;
             this.button5.Text = "Kontrola Wypożyczeń";
             this.button5.UseVisualStyleBackColor = true;
@@ -110,7 +111,7 @@
             // button6
             // 
             this.button6.Location = new System.Drawing.Point(226, 39);
-            this.button6.Name = "ButtonWyloguj";
+            this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(117, 46);
             this.button6.TabIndex = 3;
             this.button6.Text = "Wyloguj";
@@ -119,16 +120,26 @@
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(226, 91);
-            this.pictureBox1.Name = "PictureBox";
+            this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(116, 142);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(226, 262);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(116, 48);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Wyszukaj";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // AdminMainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(373, 341);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -154,5 +165,6 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button7;
     }
 }
